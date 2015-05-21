e267f0dd1317395a915bb1df2dde1ec9dd5375fe
Stein Karlsen <karlsen.stein@gmail.com>
2015-05-21 08:41:24 +0200

    Node filter initial commit

--- code_visualizer/ast_parser/node_filter.py
+++ code_visualizer/ast_parser/node_filter.py
@@ -3,38 +3,23 @@
 
 from abc import ABCMeta, abstractmethod
 
-
-"""
-Takes a dictionary of nodes with the following structure:
-    {
-        'name' : {
-            'type': '<type>',
-            'line_number': XXX,
-            'col_number' : YYY,
-            'structure' : {},
-            'children' : {
-
-            }
-        }
-    }
-where the 'children' key contain 0 or more node-dictionaries.
-
-Returns the fields queried for from the nodes matching the filter.
-"""
-
-class NodeFilter(object):
-
-    def __init__(self, node_tree):
-        self.node_tree = node_tree
-
-    def filter(self, criteria):
-        assert(isinstance(criteria, Criteria))
-
-
 class Criteria(object):
+    """
+    Base-class for filtering criteria.
+    """
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def match(self):
-        return False
+    def return_node(self, node_parents_types, node):
+        return True
 
+    @abstractmethod
+    def visit_children(self, node_parents_types, node):
+        return True
+
+class PassThroughCriteria(Criteria):
+    def return_node(self, node_parents_types, node):
+        return True
+
+    def visit_children(self, node_parents_types, node):
+        return True
