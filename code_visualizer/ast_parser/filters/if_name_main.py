ada18140d7bef4b19560d122973f9744b617c25c
Stein Karlsen <karlsen.stein@gmail.com>
2016-03-27 20:41:23 +0200

    Refactor: moved IfMainFilter to separate module

---
+++ code_visualizer/ast_parser/filters/if_name_main.py
@@ -0,0 +1,66 @@
+import ast
+
+from ast_parser.handlers.if_handler import IfHandler
+from ast_parser.node_filter import Criteria
+from ast_parser.node_utils import get_node_name, get_node_value
+
+
+class IfMainFilter(Criteria):
+    """
+    Filter which visits "if __name__ == '__main__' statements
+    """
+
+    def __init__(self):
+        self.main_body = []
+        self.if_handler = IfHandler()
+
+    def wants_to_visit_descendants(self, node_parents, node):
+        """Only visit top level nodes
+
+        :param node_parents: string of parent-nodes
+        :type node_parents: str
+
+        :param node: current node
+        :type node: ast.Ast
+
+        :return True if children of node should be visited
+        :return False if children of node should not be visited
+        """
+        return node_parents is None
+
+    def handle_node(self, node_parents, node):
+        """Adds node body to list of main-bodies if it matches the criteria.
+
+        :param node_parents: string of node parents
+        :param node: current node
+        """
+        if isinstance(node, self.if_handler.supported_types):
+            node_dict = self.if_handler.handle(node)
+
+            left, op, right = get_if_parts(node_dict)
+            if is_name(left) and is_equal(op) and is_main(right):
+                self.main_body.append(node)
+
+
+def get_if_parts(node_dict):
+    test = node_dict.get('test')
+    if isinstance(test, ast.Compare):
+        left_side = test.left
+        comparator = test.ops[0]
+        right_side = test.comparators[0]
+        return left_side, comparator, right_side
+    return None, None, None
+
+
+def is_name(node):
+    return isinstance(node, ast.Name) \
+           and get_node_name(node) == '__name__'
+
+
+def is_main(node):
+    return isinstance(node, ast.Str) \
+        and get_node_value(node) == '__main__'
+
+
+def is_equal(node):
+    return isinstance(node, ast.Eq)
--- code_visualizer/recipes/get_entry_points.py
+++ code_visualizer/recipes/get_entry_points.py
@@ -8,61 +8,12 @@ if __name__ == '__main__':
 Run from code_visualizer with "python[3] -m recipes.get_entry_points"
 """
 from __future__ import print_function
-import ast
 
+import ast
 import sys
 
-from ast_parser.handlers.if_handler import IfHandler
-from ast_parser.node_utils import get_node_name, get_node_value
+from ast_parser.filters.if_name_main import IfMainFilter
 from ast_parser.node_visitor import NodeVisitor
-from ast_parser.node_filter import Criteria
-
-
-class _IfMainFilter(Criteria):
-    """
-    Filter which visits "if __name__ == '__main__' statements
-    """
-
-    def __init__(self):
-        self.has_main = False
-        self.if_handler = IfHandler()
-
-    def wants_to_visit_descendants(self, node_parents, node):
-        return True
-
-    def handle_node(self, node_parents, node):
-        """
-        Adds the module to modules_with_main
-        if it has a main declaration.
-
-        :param node_parents: string of node parents
-        :param node: current node
-        """
-        if isinstance(node, self.if_handler.supported_types):
-            left, op, right = self.get_if_parts(node)
-            if _IfMainFilter.if_eq_main(left, op, right):
-                self.has_main = True
-
-    def get_if_parts(self, node):
-        node_dict = self.if_handler.handle(node)
-        test = node_dict.get('test')
-        if isinstance(test, ast.Compare):
-            left_side = test.left
-            comparator = test.ops[0]
-            right_side = test.comparators[0]
-            return left_side, comparator, right_side
-        return None, None, None
-
-    @staticmethod
-    def if_eq_main(left, op, right):
-        if isinstance(left, ast.Name)\
-                and get_node_name(left) == '__name__' \
-                and isinstance(op, ast.Eq) \
-                and isinstance(right, ast.Str) \
-                and get_node_value(right) == '__main__':
-            return True
-        else:
-            return False
 
 
 def print_main(path_to_python_file):
@@ -77,11 +28,11 @@ def print_main(path_to_python_file):
 
     ast_tree = ast.parse(python_file_as_string)
 
-    if_main_filter = _IfMainFilter()
+    if_main_filter = IfMainFilter()
     if_statement_visitor = NodeVisitor(if_main_filter)
     if_statement_visitor.visit(ast_tree)
-    if if_main_filter.has_main:
-        print('{0}'.format(path_to_python_file))
+    if if_main_filter.main_body:
+        print('{0} - at line {1}'.format(path_to_python_file, [m.lineno for m in if_main_filter.main_body]))
 
 
 if __name__ == '__main__':
