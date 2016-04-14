799a2a50db504a15678b44dcf0dbdd039421fc63
Stein Karlsen <karlsen.stein@gmail.com>
2016-04-14 20:02:24 +0200

    Using argparse

--- code_visualizer/__main__.py
+++ code_visualizer/__main__.py
@@ -4,20 +4,29 @@
 Main class that prints the python files of a directory and sub-directories
 """
 from __future__ import print_function
-import sys
+
+import argparse
 
 from utils.path_walker import get_directory_structure
 
 
-def main(base_path):
+def main(input_base_path, output_path=None):
     """
     Iterates over found python files and prints file-path and file-name
     """
-    for file_path, file_name in get_directory_structure(base_path):
+    for file_path, file_name in get_directory_structure(input_base_path):
         print(file_path, file_name)
 
+
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        main(base_path=sys.argv[1])
-    else:
-        print("%s <directory to analyze>" % sys.argv[0])
+    parser = argparse.ArgumentParser(
+        description='Visualize dependencies and structure of python code')
+
+    parser.add_argument('--output_dir', help='The directory where files are created.')
+    parser.add_argument('input_dir', help='The base path for the code to visualize.')
+
+    args = parser.parse_args()
+
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+    main(input_dir, output_dir)
